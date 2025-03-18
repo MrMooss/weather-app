1dd9988bb7543cb07d7f435a44e9ca5d1eaa29a3
szabo8 <Attila.Szabo2@kuka.com>
2025-03-18 07:26:15 +0100

    Progression: Loops and events

--- Learning/01-starting-project/src/app/app.component.ts
+++ Learning/01-starting-project/src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './user/dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedId?: string;
+
+  get selectedUser()
+  {
+    return DUMMY_USERS.find((user) => user.id === this.selectedId);
+  }
+  onSelectUser(id: string) {
+    this.selectedId = id
+  }
+}
--- Learning/01-starting-project/src/app/user/user.component.ts
+++ Learning/01-starting-project/src/app/user/user.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-import { DUMMY_USERS } from './dummy-users';
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
 
-const rndIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -11,5 +19,14 @@ const rndIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  public selected =  DUMMY_USERS[rndIndex]
+  @Input({required: true}) user!: User;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
 }
